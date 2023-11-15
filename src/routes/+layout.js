d178d533ac7e5d2d2373e1f35a809033383b3fb3
Brian Cairns <bcairns@logitech.com>
2023-11-15 01:45:44 -0700

    prerender, and remove husky

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -3,3 +3,9 @@
  * effectively turning it into a Single-Page Application (SPA), rendering purely in the browser.
  */
 export const ssr = false;
+
+/**
+ * This prerenders the application at build time, so the entire app can be deployed as static files.
+ * This is needed for adapter-static to work.
+ */
+export const prerender = true;
